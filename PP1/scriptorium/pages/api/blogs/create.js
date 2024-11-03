4337d07ee1b27030a9bcfbc1016d4344d5be5f2b
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-11-03 11:08:37 -0500

    Blog create, edit & delete implementations

--- PP1/scriptorium/pages/api/blogs/create.js
+++ PP1/scriptorium/pages/api/blogs/create.js
@@ -1,16 +1,22 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { title, description, content, tags, userId } = req.body;
+import prisma from "@/utils/db";
+import auth from "@/utils/auth";
 
-        try {
-            const blogPost = await prisma.blogPost.create({
-                data: { title, description, content, tags, userId },
-            });
-            res.status(201).json(blogPost);
-        } catch (error) {
-            res.status(400).json({ error: 'Failed to create blog post' });
+export default async function handler(req, res) {
+    await auth(req, res, async () => {
+        if (req.method === 'POST') {
+            const { title, description, content, tags } = req.body;
+            const userId = req.user.userId;
+    
+            try {
+                const blogPost = await prisma.blogPost.create({
+                    data: { title, description, content, tags, userId },
+                });
+                res.status(201).json(blogPost);
+            } catch (error) {
+                res.status(400).json({ error: 'Failed to create blog post' });
+            }
+        } else {
+            res.status(405).json({ error: 'Method not allowed' });
         }
-    } else {
-        res.status(405).json({ error: 'Method not allowed' });
-    }
+    }); 
 }
