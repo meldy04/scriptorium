766bb7881c1f394b4a44bf95770dccc02e86ba81
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-11-03 18:02:47 -0500

    GET template

--- PP1/scriptorium/pages/api/templates/[id].js
+++ PP1/scriptorium/pages/api/templates/[id].js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
       });
       res.status(200).json(template);
     } catch (error) {
-      console.log(error);
       res.status(404).json({ error: 'Template not found' });
     }
   }
