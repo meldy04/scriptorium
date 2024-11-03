3a7e427e6d348175536721dc350cb0a9404d4e9e
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-11-03 18:02:37 -0500

    GET template

--- PP1/scriptorium/pages/api/templates/[id].js
+++ PP1/scriptorium/pages/api/templates/[id].js
@@ -1,4 +1,7 @@
+import prisma from "@/utils/db";
+
 export default async function handler(req, res) {
+  if (req.method === 'GET') {
     const { id } = req.query;
     try {
       const template = await prisma.template.findUnique({
@@ -6,7 +9,8 @@ export default async function handler(req, res) {
       });
       res.status(200).json(template);
     } catch (error) {
+      console.log(error);
       res.status(404).json({ error: 'Template not found' });
     }
   }
-  
+}
