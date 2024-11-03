4beb6be8241b08e99802e0c807ff9fb1b1d85017
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-11-03 10:37:47 -0500

    Debugged edit profile

--- PP1/scriptorium/pages/api/users/edit.js
+++ PP1/scriptorium/pages/api/users/edit.js
@@ -1,3 +1,5 @@
+import prisma from "@/utils/db";
+
 export default async function handler(req, res) {
     if (req.method === 'PUT') {
         const { userId, firstName, lastName, avatar, phone } = req.body;
