dc5ce602d27012cd3b8ede4dac161a5863d5afb2
meldy04 <120117788+meldy04@users.noreply.github.com>
2024-11-18 14:18:22 -0500

    Completed E10

---
+++ E10/scripts.js
@@ -0,0 +1,12 @@
+function getAppUrl() {
+    return "https://mvgmlw2wfzzv7mo2dwbydxx7wa0zqbqb.lambda-url.us-east-2.on.aws/";
+}
+
+function getUTorId() {
+    return "bilemeld";
+}
+
+module.exports = {
+    getAppUrl,
+    getUTorId
+};
